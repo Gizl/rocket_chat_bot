194509f854aa9cb9c029b75c8a4c010484b93b2e
Vadim Fabrichnov <zorumnipa@gmail.com>
2019-03-11 18:13:16 +0300

    update test script

--- test.py
+++ test.py
@@ -4,16 +4,16 @@ from settings import DB_PATH
 
 conn = sqlite3.connect(DB_PATH, check_same_thread=False)
 cursor = conn.cursor()
-# for x in range(10):
-#     cursor.execute("""INSERT INTO channel (name)
-#                           VALUES (?)""", (x, ))
-#     cursor.execute("""INSERT INTO project (project_id, channel)
-#                           VALUES (?, ?)""", (x, x, ))
-#     cursor.execute("""INSERT INTO developer (username)
-#                             VALUES (?)""", (x, ))
-#     cursor.execute("""INSERT INTO developer_project (developer, project, impact)
-#                         VALUES (?, ?, ?)""", (x, x, x))
-#     conn.commit()
+for x in range(10):
+    cursor.execute("""INSERT INTO channel (name)
+                          VALUES (?)""", (f"channel_{x}", ))
+    cursor.execute("""INSERT INTO project (project_id, channel)
+                          VALUES (?, ?)""", (f"project_{x}", f"channel_{x}", ))
+    cursor.execute("""INSERT INTO developer (username)
+                            VALUES (?)""", (f"developer_{x}", ))
+    cursor.execute("""INSERT INTO developer_project (developer, project, impact)
+                        VALUES (?, ?, ?)""", (f"developer_{x}", f"project_{x}", x))
+    conn.commit()
 cursor.execute("""SELECT project_id, username, channel.name, impact
                     FROM project
                     INNER JOIN channel
@@ -25,3 +25,5 @@ projects = cursor.fetchall()
 channels_relations = {}
 for project_id, username, channel_name, impact in projects:
     channels_relations.setdefault(channel_name, {}).setdefault(project_id, []).append([username, impact])
+
+print(channels_relations)
