0e39bfe91eee24f4cff99472c873134a53d1b298
Vadim Fabrichnov <zorumnipa@gmail.com>
2019-03-18 16:42:37 +0300

    Fix query and while loop

--- send_messages.py
+++ send_messages.py
@@ -14,11 +14,10 @@ conn = sqlite3.connect(settings.DB_PATH, check_same_thread=False)
 cursor = conn.cursor()
 
 request_params = {"private_token": settings.GITLAB_TOKEN, "state": "opened"}
-cursor.execute("""SELECT project_id, username, gitlab, channel.name, impact
-                    FROM project
-                    INNER JOIN channel
-                    LEFT OUTER JOIN developer_project dp on project.project_id = dp.project
-                    LEFT OUTER JOIN developer d on dp.developer = d.username""")
+cursor.execute("""SELECT  pr.project_id, d.username, d.username, channel.name, dp.impact FROM channel 
+                    JOIN project pr ON channel.name = pr.channel
+                    JOIN developer_project dp on pr.project_id = dp.project
+                    JOIN developer d on dp.developer = d.username""")
 projects = cursor.fetchall()
 channels_relations = {}
 for project_id, username, gitlab, channel_name, impact in projects:
@@ -46,12 +45,15 @@ for channel_name, project_relations in channels_relations.items():
                 channel_message.setdefault(developer, []).append(merge_request[0])
                 merge_requests[i][1] -= 1
         while [x for x in merge_requests if x[1] > 0]:
-            developer = developers[randint(0, len(developers))]
-            if merge_requests[0][0].get('author').get('username') == developer[1]:
+            mr = [x for x in merge_requests if x[1] > 0][0][0]
+            developer = developers[randint(0, len(developers)-1)]
+            if mr.get('author').get('username') == developer[1]:
                 continue
-            channel_message.setdefault(developer[0], []).append(merge_requests[0][0])
-            merge_requests[0][1] -= 1
+            channel_message.setdefault(developer[0], []).append(mr)
+            i = [i for i, e in enumerate(merge_requests) if e[0]['id']==mr.get('id')]
+            merge_requests[i[0]][1] -= 1
 
     channel_message = "\n".join([f"\n@{key}: \n{', '.join([str(x.get('web_url')) for x in values])}" for key, values in channel_message.items()])
-    channel_message = f"Please, review those MRs today:\n{channel_message}"
+    channel_message = f"Please, review those MRs today:\n{channel_message}\n"
     rocket.chat_post_message(channel_message, channel=channel_name, alias='BOT NOTIFICATION')
+
