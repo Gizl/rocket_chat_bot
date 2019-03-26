94099eab87af2f4b762e31a10de611dad79eb681
Evgeny Kovalev <evgeny.kovallev@gmail.com>
2019-03-26 15:59:27 +0300

    fix check merge_requests

--- utils.py
+++ utils.py
@@ -7,16 +7,22 @@ from rocketchat_API.rocketchat import RocketChat
 def check_merged_requests_for_upvotes(channels_name: str, projects: dict):
     rocket = RocketChat(settings.ROCKET_USERNAME, settings.ROCKET_PASSWORD, server_url=settings.ROCKET_URL)
     request_params = {"private_token": settings.GITLAB_TOKEN, "state": "merged",
-                      "created_after": datetime.date.today().replace(day=datetime.date.today().day - 1)}
+                      "created_after": datetime.date.today() - datetime.timedelta(days=1)}
 
     for project_id, approvers_number in projects.items():
         url = f"{settings.GITLAB_URL}projects/{project_id}/merge_requests"
         merge_requests = requests.get(url, params=request_params).json()
 
-        channel_message = "MRs where upvotes are less than 2:\n"
+        channel_message = f"MRs where upvotes are less than {approvers_number}:\n"
+
+        flag = False
 
         for merge_request in merge_requests:
             if merge_request.get('upvotes') < approvers_number:
+                flag = True
                 channel_message += merge_request.get('web_url') + '\n'
 
-        rocket.chat_post_message(channel_message, channel=channels_name, alias='BOT NOTIFICATION')
+        if flag:
+            rocket.chat_post_message(channel_message, channel=channels_name, alias='BOT NOTIFICATION')
+        else:
+            continue
