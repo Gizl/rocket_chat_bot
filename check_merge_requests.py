9ee0e0f8798fe07708ada209a3d3ebf649af0e50
Ibrokhim Tursunov <ibragim1989@gmail.com>
2019-03-28 17:14:34 +0300

    Change constants from settings.py to environment variables, beautify code accroding to PEP-8

--- check_merge_requests.py
+++ check_merge_requests.py
@@ -1,30 +1,29 @@
 from datetime import datetime, timedelta
 import re
-import sys
 import logging
 
 import gitlab
 from jira import JIRA, exceptions as jira_exceptions
 
-from settings1 import GITLAB_TOKEN, JIRA_NAME, JIRA_PASSWORD, JIRA_BASE_URL, JIRA_TASK_STATUS, TIME_WINDOW_IN_HOURS, \
+from settings import GITLAB_TOKEN, GITLAB_URL, JIRA_NAME, JIRA_PASSWORD, JIRA_BASE_URL, JIRA_TASK_STATUS, TIME_WINDOW_IN_HOURS, \
                         REGULAR_STRING, REGULAR_STRING_TASK
 
 def proceed_gitlab_merge_requests():
     """
-    The function return a list of url of jira tasks, which should be closed
+    The function return a list of urls of jira tasks, which should be processed.
     :param token: personal token from Gitlab profile
     :param t: how many hours to the past to proceed Gitlab merges
     :return: list of URLS of Jira tasks, which should be closed
     """
     regular_expression = re.compile(REGULAR_STRING)
     try:
-        user = gitlab.Gitlab("https://gitlab.com", private_token=GITLAB_TOKEN)
+        user = gitlab.Gitlab(GITLAB_URL, private_token=GITLAB_TOKEN)
         user.auth()
         merges = user.mergerequests.list()
     except (gitlab.exceptions.GitlabAuthenticationError, gitlab.exceptions.GitlabHttpError) as error:
         now = datetime.now().replace(microsecond=0)
         logging.error("Unable to login with provided token. {0}".format(now))
-        sys.exit(1)
+        return set()
 
     jira_urls = set()
     t_now = datetime.now()
@@ -33,7 +32,7 @@ def proceed_gitlab_merge_requests():
     for merge in merges:
         urls = re.findall(regular_expression, merge.description)
         for url in urls:
-            t_updated = datetime.strptime(merge.updated_at[:19], "%Y-%m-%dT%H:%M:%S")
+            t_updated = datetime.strptime(merge.updated_at, "%Y-%m-%dT%H:%M:%S.%fZ")
             if t_updated > t_edge:
                 jira_urls.add(url)
     return jira_urls
@@ -47,7 +46,7 @@ def proceed_jira_tasks(jira_urls):
     :param pw: You password in JIRA. Be careful! If you provided incorrect password, it might triggering capture and you won't be able to turn it off after.
     :return: None
     """
-    options = {"server" : JIRA_BASE_URL}
+    options = {"server": JIRA_BASE_URL}
     jira = JIRA(options, basic_auth=(JIRA_NAME, JIRA_PASSWORD))
     regular_expression = re.compile(REGULAR_STRING_TASK)
     now = datetime.now().replace(microsecond=0)
@@ -55,14 +54,16 @@ def proceed_jira_tasks(jira_urls):
     for url in jira_urls:
         try:
             task_id = re.findall(regular_expression, url)[0][1]
+        except IndexError:
+            logging.error("Couldn't find task ID in URL {0} {1}".format(url, now))
+
+        try:
             for action in jira.transitions(task_id):
-                if action.get('name','') == JIRA_TASK_STATUS:
+                if action.get('name', '') == JIRA_TASK_STATUS:
                     transition_id = int(action['id'])
                     jira.transition_issue(task_id, transition_id)
                     logging.info("Task {0} was updated to '{1}' status {2}".format(task_id, JIRA_TASK_STATUS, now))
                     break
-        except IndexError:
-            logging.error("Couldn't find task ID in URL {0} {1}".format(url, now))
         except jira_exceptions.JIRAError:
             logging.error("Couldn't proceed provided task {0} {1}".format(task_id, now))
 
--- settings.py
+++ settings.py
@@ -1,18 +1,14 @@
-JIRA_NAME = ""
-JIRA_PASSWORD = r""
-JIRA_BASE_URL = "https://advinow.atlassian.net"
-JIRA_TASK_STATUS = "RFQA"
-
-TIME_WINDOW_IN_HOURS = 24
-REGULAR_STRING = r"{0}/browse/.*".format(JIRA_BASE_URL)
-REGULAR_STRING_TASK = r"({0}/browse/)(.*)$".format(JIRA_BASE_URL)
-=======
 import os
 
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+# JIRA
+JIRA_NAME = os.getenv("JIRA_NAME")
+JIRA_PASSWORD = os.getenv("JIRA_PASSWORD")
+JIRA_BASE_URL = os.getenv("JIRA_BASE_URL")
+JIRA_TASK_STATUS = os.getenv("JIRA_TASK_STATUS")
 # DATABASE
 DB_PATH = os.getenv("DB_PATH")
 # GITLAB
@@ -24,4 +20,8 @@ ROCKET_USERNAME = os.getenv("ROCKET_USERNAME")
 ROCKET_PASSWORD = os.getenv("ROCKET_PASSWORD")
 ROCKET_TOKEN = os.getenv("ROCKET_TOKEN")
 ROCKET_ID = os.getenv("ROCKET_ID")
+# ADDITIONAL PARAMETERS
+TIME_WINDOW_IN_HOURS = 24
+REGULAR_STRING = r"{0}/browse/.*".format(JIRA_BASE_URL)
+REGULAR_STRING_TASK = r"({0}/browse/)(.*)$".format(JIRA_BASE_URL)
 
